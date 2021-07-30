e4cb51d6b4432f187e225103ac4ae5887e06deff
tvergara <tverg419@gmail.com>
2021-07-30 17:49:36 -0400

    added draw func

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 const gameBoard = document.querySelector('.game-board')
 const squares = document.querySelectorAll('.square')
-const width = 10;
-const height = 5;
+
+// Stores board dimensions
+const boardWidth = 10;
+const boardHeight = 5;
 
 /*
 Each tetrimino should have 4 rotations and they should be in the same index 
@@ -11,130 +13,130 @@ indicates that square should be colored and a 0 indicates no color
 */
 const iTetrimino = [
     [[0,0,0,0],
-     [1,1,1,1],
-     [0,0,0,0],
-     [0,0,0,0]],
-
+    [1,1,1,1],
+    [0,0,0,0],
+    [0,0,0,0]],
+    
     [[0,0,1,0],
-     [0,0,1,0],
-     [0,0,1,0],
-     [0,0,1,0]],
-
+    [0,0,1,0],
+    [0,0,1,0],
+    [0,0,1,0]],
+    
     [[0,0,0,0],
-     [0,0,0,0],
-     [1,1,1,1],
-     [0,0,0,0]],
-
+    [0,0,0,0],
+    [1,1,1,1],
+    [0,0,0,0]],
+    
     [[0,1,0,0],
-     [0,1,0,0],
-     [0,1,0,0],
-     [0,1,0,0]]
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0]]
 ]
 const jTetrimino = [
     [[0,1,0],
-     [0,1,0],
-     [1,1,0]],
-
+    [0,1,0],
+    [1,1,0]],
+    
     [[1,0,0],
-     [1,1,1],
-     [0,0,0]],
-
+    [1,1,1],
+    [0,0,0]],
+    
     [[0,1,1],
-     [0,1,0],
-     [0,1,0]],
-
+    [0,1,0],
+    [0,1,0]],
+    
     [[1,1,1],
-     [0,0,1],
-     [0,0,0]]
+    [0,0,1],
+    [0,0,0]]
 ]
 const lTetrimino = [
     [[0,1,0],
-     [0,1,0],
-     [0,1,1]],
-
+    [0,1,0],
+    [0,1,1]],
+    
     [[0,0,0],
-     [1,1,1],
-     [1,0,0]],
-
+    [1,1,1],
+    [1,0,0]],
+    
     [[1,1,0],
-     [0,1,0],
-     [0,1,0]],
-
+    [0,1,0],
+    [0,1,0]],
+    
     [[0,0,1],
-     [1,1,1],
-     [0,0,0]]    
+    [1,1,1],
+    [0,0,0]]    
 ]
 const oTetrimino = [
     [[0,0,0,0],
-     [0,1,1,0],
-     [0,1,1,0],
-     [0,0,0,0]],
-
+    [0,1,1,0],
+    [0,1,1,0],
+    [0,0,0,0]],
+    
     [[0,0,0,0],
-     [0,1,1,0],
-     [0,1,1,0],
-     [0,0,0,0]],
-
+    [0,1,1,0],
+    [0,1,1,0],
+    [0,0,0,0]],
+    
     [[0,0,0,0],
-     [0,1,1,0],
-     [0,1,1,0],
-     [0,0,0,0]],
-
+    [0,1,1,0],
+    [0,1,1,0],
+    [0,0,0,0]],
+    
     [[0,0,0,0],
-     [0,1,1,0],
-     [0,1,1,0],
-     [0,0,0,0]]
+    [0,1,1,0],
+    [0,1,1,0],
+    [0,0,0,0]]
 ]
 const sTetrimino = [
     [[0,0,0],
-     [0,1,1],
-     [1,1,0]],
-
+    [0,1,1],
+    [1,1,0]],
+    
     [[1,0,0],
-     [1,1,0],
-     [0,1,0]],
-
+    [1,1,0],
+    [0,1,0]],
+    
     [[0,0,0],
-     [0,1,1],
-     [1,1,0]],
-
+    [0,1,1],
+    [1,1,0]],
+    
     [[1,0,0],
-     [1,1,0],
-     [0,1,0]]
+    [1,1,0],
+    [0,1,0]]
 ]
 const tTetrimino = [
     [[0,0,0],
-     [1,1,1],
-     [0,1,0]],
-
+    [1,1,1],
+    [0,1,0]],
+    
     [[0,1,0],
-     [1,1,0],
-     [0,1,0]],
-
+    [1,1,0],
+    [0,1,0]],
+    
     [[0,1,0],
-     [1,1,1],
-     [0,0,0]],
-
+    [1,1,1],
+    [0,0,0]],
+    
     [[0,1,0],
-     [0,1,1],
-     [0,1,0]]
+    [0,1,1],
+    [0,1,0]]
 ]
 const zTetrimino = [
     [[1,1,0],
-     [0,1,1],
-     [0,0,0]],
-
+    [0,1,1],
+    [0,0,0]],
+    
     [[0,0,1],
-     [0,1,1],
-     [0,1,0]],
-
+    [0,1,1],
+    [0,1,0]],
+    
     [[1,1,0],
-     [0,1,1],
-     [0,0,0]],
-
+    [0,1,1],
+    [0,0,0]],
+    
     [[0,0,1],
-     [0,1,1],
-     [0,1,0]]
+    [0,1,1],
+    [0,1,0]]
 ]
 
 const tetriminoes = [iTetrimino, jTetrimino, lTetrimino, oTetrimino, sTetrimino, tTetrimino, zTetrimino]
@@ -148,7 +150,12 @@ const tetriminoColors = {
     zTetrimino: 'red'
 }
 
-// function to create (width * height)
+// Stores current information
+let currentTetrimino = tetriminoes[0][0]
+let currentPosition = 4 // Starts at the top of the grid
+let currentColor = tetriminoColors[tetriminoes[0]]
+
+// function to create grid(width * height)
 function createGrid(width, height) {
     for (let i = 0; i < height; i++) {
         for (let j = 0; j < width; j++) {
@@ -159,6 +166,24 @@ function createGrid(width, height) {
     }
 }
 
-document.addEventListener('DOMContentLoaded', createGrid(width, height))
-console.log(tetriminoColors)
+function drawTetrimino() {
+    currentTetrimino.forEach(square => {
+        for (let i = 0; i < square.length; i++) {
+            for(let j = 0; j < square.length; j++) {
+                if (currentTetrimino[i][j] === 1) {
+                    squares[currentPosition + (10 * i) + j].style.background = 'red'
+                } else {
+                    squares[currentPosition + (10 * i) + j].style.background = 'white'
+                }
+            }
+        }
+    })
+}
+function undraw() {
+
+}
+
+document.addEventListener('DOMContentLoaded', createGrid(boardWidth, boardHeight))
+drawTetrimino()
+console.log(currentTetrimino[1][1])
 console.log('Working')
