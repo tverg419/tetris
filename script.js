1c107e2d38d1758b3365d9d02a6866fe13daf4ac
tvergara <tverg419@gmail.com>
2021-07-30 22:56:13 -0400

    added some game logic + fixed styling

--- script.js
+++ script.js
@@ -140,38 +140,32 @@ const zTetrimino = [
 ]
 
 const tetriminoes = [iTetrimino, jTetrimino, lTetrimino, oTetrimino, sTetrimino, tTetrimino, zTetrimino]
-const tetriminoColors = {
-    iTetrimino: 'cyan',
-    jTetrimino: 'blue',
-    lTetrimino: 'orange',
-    oTetrimino: 'yellow',
-    sTetrimino: 'green',
-    tTetrimino: 'purple',
-    zTetrimino: 'red'
-}
+const tetriminoColors = ['cyan','blue','orange','yellow','green','purple','red']
 
 // Stores current information
-let currentTetrimino = tetriminoes[0][0]
 let currentPosition = 4 // Starts at the top of the grid
-let currentColor = tetriminoColors[tetriminoes[0]]
+let randomNumber = Math.floor(Math.random()*tetriminoes.length)
+let randomRotation = Math.floor(Math.random()*tetriminoes[0].length)
+let currentTetrimino = tetriminoes[randomNumber][randomRotation]
+let currentColor = tetriminoColors[randomNumber]
 
 // function to create grid(width * height)
-function createGrid(width, height) {
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
-            let div = document.createElement('div')
-            div.classList.add('square')
-            gameBoard.appendChild(div)
-        }
-    }
-}
+// function createGrid(width, height) {
+//     for (let i = 0; i < height; i++) {
+//         for (let j = 0; j < width; j++) {
+//             let div = document.createElement('div')
+//             div.classList.add('square')
+//             gameBoard.appendChild(div)
+//         }
+//     }
+// }
 
 function drawTetrimino() {
     currentTetrimino.forEach(square => {
         for (let i = 0; i < square.length; i++) {
             for(let j = 0; j < square.length; j++) {
                 if (currentTetrimino[i][j] === 1) {
-                    squares[currentPosition + (10 * i) + j].style.background = 'red'
+                    squares[currentPosition + (10 * i) + j].style.background = 'cyan'
                 } else {
                     squares[currentPosition + (10 * i) + j].style.background = 'white'
                 }
@@ -179,11 +173,30 @@ function drawTetrimino() {
         }
     })
 }
-function undraw() {
+function undrawTetrimino() {
+    currentTetrimino.forEach(square => {
+        for (let i = 0; i < square.length; i++) {
+            for(let j = 0; j < square.length; j++) {
+                squares[currentPosition + (10 * i) + j].style.background = 'white'
+            }
+        }
+    })
+}
+drawTetrimino()
+// Movement Functions
+function moveDown() {
+    undrawTetrimino()
+    currentPosition += boardWidth
+    drawTetrimino()
+}
+function moveLeft() {
+
+}
+function moveRight() {
 
 }
 
-document.addEventListener('DOMContentLoaded', createGrid(boardWidth, boardHeight))
-drawTetrimino()
-console.log(currentTetrimino[1][1])
+document.addEventListener('DOMContentLoaded', function () {
+    // createGrid(boardWidth, boardHeight)
+    setInterval(moveDown(), 1000)})
 console.log('Working')
